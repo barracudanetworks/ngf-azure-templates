bc769316488b25508dc2d2bb5bacb409d8ea2dd9
Gemma Allen <gallen@barracuda.com>
2019-01-09 09:54:27 +0000

    testing build path

--- contrib/NGF-Quickstart-HA-1NIC-AS-ELB-BASIC/test/invokePester.ps1
+++ contrib/NGF-Quickstart-HA-1NIC-AS-ELB-BASIC/test/invokePester.ps1
@@ -3,6 +3,7 @@ param (
 )
 
 $SourceDir = "$env:BUILD_SOURCESDIRECTORY\$templatename"
+Write-Host ($env:BUILD_SOURCESDIRECTORY)
 $TempDir = $env:TEMP
  
 $modulePath = Join-Path $TempDir Pester-master\Pester.psm1
