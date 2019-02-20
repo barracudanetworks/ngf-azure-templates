8b9912c0d297f8260f346022e9cef30d445a08e6
Gemma Allen <32898854+gallen-bar@users.noreply.github.com>
2019-02-20 11:20:53 +0000

    New Control Center custom deployment template

---
+++ contrib/CGF-Custom-ControlCenter/deploy.ps1
@@ -0,0 +1,10 @@
+﻿$password = Read-Host -AsSecureString 'Please provide password for NGF, WAF and Web Server
+!! BEWARE: Password complexity rules 12 characters, [A-Za-z0-9] and special char !!' 
+
+Write-Host "`nCreating Resource Group $rg for the networking configuration"
+New-AzureRMResourceGroup -Name $rg -Location "$location"
+
+Write-Host "Deploying Barracuda Next Gen Firewall F Series"
+New-AzureRMResourceGroupDeployment -Name "Deploy_$rg" -ResourceGroupName "$rg" `
+    -TemplateFile "azuredeploy.json" -TemplateParameterFile "azuredeploy.parameters.json" `
+    -adminPassword $password 
